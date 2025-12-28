7dfd12019817861529eaed9fc78123e8a59e37cf
AhasnaCharumee <charumeeahasna@gmail.com>
2025-12-28 11:44:17 +0530

    rebuild: update dist for port 3000

--- dist/index.js
+++ dist/index.js
@@ -32,7 +32,7 @@ app.use("/api/admin", admin_1.default);
 // Error handling middleware
 app.use(errorHandler_1.errorHandler);
 // Server start
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
 });
