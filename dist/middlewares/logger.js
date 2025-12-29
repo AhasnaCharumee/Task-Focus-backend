7b29ba585267c0b6f794773dac7a843a98171eba
AhasnaCharumee <charumeeahasna@gmail.com>
2025-12-29 09:33:36 +0530

    build: clean dist and recompile for OAuth routes

--- dist/middlewares/logger.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logger = void 0;
-const logger = (req, res, next) => {
-    const now = new Date().toISOString();
-    console.log(`[${now}] ${req.method} ${req.originalUrl}`);
-    next();
-};
-exports.logger = logger;
